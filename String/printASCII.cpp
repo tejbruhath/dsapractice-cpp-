102a0d7ce79c185f4b4be27ece2899c4aedeb8c7
tejbruhath <tejbruhath001@gmail.com>
2025-03-17 14:01:39 +0530

    tpgitcd

---
+++ String/printASCII.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c;
+    cout<< "Enter your character:"<<endl;
+    cin>> c;
+    cout<< "ASCII value of your character is :"<< int(c)<<endl;
+    return 0;
+}
